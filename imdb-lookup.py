c722da4c8bf4f744f1707de06c1460027a33c9b6
Shivam Gohel <shivamgohel777@gmail.com>
2017-12-31 06:07:10 +0530

    Update imdb-lookup.py
    Changes made:
    1) OMDB just went private, so added appended the apikey with the url
    2) New Tags added
    3) Year changed. ( i.e  extended to 2018)

--- imdb-lookup.py
+++ imdb-lookup.py
@@ -16,7 +16,7 @@ import webbrowser
 import sys
 
 def finder(name):
-    replace = [".avi","1.4","5.1","-","DVDRip","BRRip","XviD","1CDRip","aXXo","[","]","(",")","{","}","{{","}}"
+    replace = [".avi","1.4","5.1","-","_","DVDRip","BRRip","XviD","1CDRip","aXXo","[","]","(",")","{","}","{{","}}"
     "x264","720p","StyLishSaLH (StyLish Release)","DvDScr","MP3","HDRip","WebRip",
     "ETRG","YIFY","StyLishSaLH","StyLish Release","TrippleAudio","EngHindiIndonesian",
     "385MB","CooL GuY","a2zRG","x264","Hindi","AAC","AC3","MP3"," R6","HDRip","H264","ESub","AQOS",
@@ -30,35 +30,37 @@ def finder(name):
     "XVid","sC0rp","PTpower","OSCARS","DXVA","MXMG","3LT0N","TiTAN","4PlayHD","HQ","HDRiP","MoH","MP4","BadMeetsEvil",
     "XViD","3Li","PTpOWeR","3D","HSBS","CC","RiPS","WEBRip","R5","PSiG","'GokU61","GB","GokU61","NL","EE","Rel","NL",
     "PSEUDO","DVD","Rip","NeRoZ","EXTENDED","DVDScr","xvid","WarrLord","SCREAM","MERRY","XMAS","iMB","7o9",
-    "Exclusive","171","DiDee","v2"
+    "Exclusive","171","DiDee","v2","YTS","AG"
 
     ]
     year=0
-    for y in range(1900,2014):
+    for y in range(1900,2018):
         if str(y) in name:
             name = name.replace(str(y)," ")
             year = y
             break
     for value in replace:
         name = name.replace(value," ")
-
     name = name.strip();
-
+    key="6c909847"
     if year!=0:
-        url = "http://www.omdbapi.com/?t="+name+"&y="+str(year)
+        url = "http://www.omdbapi.com/?t="+name+"&y="+str(year)+"&apikey="+key
         response = urllib.urlopen(url).read()
         jsonvalues = json.loads(response)
+        print(str(jsonvalues))
         if jsonvalues["Response"]=="True":
-            movieurl = "www.imdb.com/title/" + jsonvalues['imdbID']
+            movieurl = "http://www.imdb.com/title/" + jsonvalues['imdbID']
             webbrowser.open_new_tab(movieurl)
 
     else:
-        url = "http://www.omdbapi.com/?t="+name
+        url = "http://www.omdbapi.com/?t="+name+"&apikey="+key
         response = urllib.urlopen(url).read()
         jsonvalues = json.loads(response)
         if jsonvalues["Response"]=="True":
-            movieurl = "www.imdb.com/title/" + jsonvalues['imdbID']
+            movieurl = "http://www.imdb.com/title/" + jsonvalues['imdbID']
             webbrowser.open_new_tab(movieurl)
 
+file = sys.argv[1].split("/")[-1]
+finder(file)
 file = sys.argv[1].split("\\")[-1]
 finder(file)
